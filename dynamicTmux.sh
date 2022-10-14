7feaaf001603d5313ae7fcf8cfe64d9fb54cf7be
NikeshSapkotaLf <nikeshsapkota@lftechnology.com>
2022-10-14 13:33:18 +0545

    tmux setup init

---
+++ dynamicTmux.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SESSION='trayt'
+SESSION_EXISTS=$(tmux list-sessions | grep $SESSION)
+
+PROJECT_ROOT="$HOME/lftechnology/trayt/"
+PROJECTS=($(ls -d "$PROJECT_ROOT"*))
+
+if [ "$SESSION_EXISTS" == "" ]; then
+  for i in ${!PROJECTS[@]}
+  do
+    PROJECT_PATH=${PROJECTS[$i]}
+    PROJECT_NAME=$(basename $PROJECT_PATH)
+
+    if [ $i -eq 0 ]; then
+      tmux new-session -s $SESSION -c $PROJECT_PATH -d
+      tmux rename-window $PROJECT_NAME
+    else
+      tmux new-window -n $PROJECT_NAME -c $PROJECT_PATH -d
+    fi
+
+    tmux split-window -h -t $PROJECT_NAME -c $PROJECT_PATH -d
+  done
+fi
+
+tmux attach-session -t $SESSION
